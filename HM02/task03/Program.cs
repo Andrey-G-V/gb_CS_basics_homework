c54ba10710115a8c4f7013669756e349fcaed80a
Andrey V <evi696@mail.ru>
2023-02-21 20:49:40 +0500

    Home work #2 Task 3 added

---
+++ HM02/task03/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int NumberRequest(string massadge)
+{
+    Console.WriteLine(massadge);
+    string number = Console.ReadLine();
+    int value = Convert.ToInt32(number);
+    return value;
+}
+
+bool checkNumber (int weekDayNum)
+{
+    if (weekDayNum > 0 && weekDayNum <= 7)
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Не верное число!");
+        return false;
+    }
+}
+
+int weekDay = NumberRequest("Введите день недели(1-7) ");
+
+if (checkNumber(weekDay))
+{
+    if (weekDay > 5)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Будний");
+    }
+}
