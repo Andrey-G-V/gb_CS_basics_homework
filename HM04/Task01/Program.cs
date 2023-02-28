fca92e50343147ca668e017272b6cddf4d604e5e
Andrey V <evi696@mail.ru>
2023-02-28 20:40:10 +0500

    Home work #4 Task 1 added

---
+++ HM04/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int CheckInput (string messege)
+{
+    Console.Write($"{messege} > ");
+    int value;
+    bool isCorrect = int.TryParse(Console.ReadLine(), out value);
+    if (isCorrect)
+    {
+        return value;
+    }
+    Console.WriteLine("fatal error: Вы ввели не число");
+    Environment.Exit(1);
+    return (0);
+}
+
+int Power(int number, int exponent)
+{
+    int power = 1;
+    for (int i = 1; i <= exponent; i++)
+    {
+        power = power * number;
+    }
+    return power;
+}
+
+int number = CheckInput("Введите число");
+int exponent = CheckInput("Введите степень");
+Console.WriteLine($"Число {number} в степени {exponent} равно {Power(number, exponent)}");
