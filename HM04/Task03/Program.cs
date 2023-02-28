71dff7e27ad91f5d32251540df78498149310f44
Andrey V <evi696@mail.ru>
2023-02-28 21:34:36 +0500

    Home work #4 Task 3 added

---
+++ HM04/Task03/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]6, 1, 33 -> [6, 1, 33]
+
+
+// int[] GenerateArrey(int[] arrey)
+// {
+//     for(int i = 0; i < arrey.Length; i++)
+//     {
+//         arrey[i] = new Random().Next(1 ,100);
+//     }
+//     return arrey;
+// }
+
+void PrintArrey(int[] collec){
+    int count = collec.Length - 1;
+    int position = 0;
+    Console.Write("[");
+    while(position < count){
+
+        Console.Write($"{collec[position]} ");
+        position++;
+    }
+    Console.Write(collec[collec.Length - 1]);
+    Console.WriteLine("]");
+}
+
+int[] arrey = new int[8];
+
+for(int i = 0; i < arrey.Length; i++)
+{
+    arrey[i] = new Random().Next(1 ,100);
+}
+
+// GenerateArrey(arrey);
+PrintArrey(arrey);
