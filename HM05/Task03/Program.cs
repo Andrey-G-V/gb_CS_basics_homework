f68981e3ccf78f33b5aaa9daa6b30014757c3550
Andrey V <evi696@mail.ru>
2023-03-07 20:55:52 +0500

    Home work #6 Task 1 added

--- HM05/Task03/Program.cs
+++ HM05/Task03/Program.cs
@@ -2,27 +2,44 @@
 // [3 7 22 2 78] -> 76
 
 double[] arrayRealNumbers = new double[5];
-  for (int i = 0; i < arrayRealNumbers.Length; i++ )
-  {
-    arrayRealNumbers[i] = new Random().Next(1, 100);
-    Console.Write(arrayRealNumbers[i] + " ");
-  }
+for (int i = 0; i < arrayRealNumbers.Length; i++)
+{
+    arrayRealNumbers[i] = new Random().Next(1, 100) + new Random().NextDouble();
+    Console.Write($"{arrayRealNumbers[i]:f3}   ");
+}
 
 double maxNumber = arrayRealNumbers[0];
 double minNumber = arrayRealNumbers[0];
 
-  for (int i = 1; i < arrayRealNumbers.Length; i++)
-  {
+for (int i = 1; i < arrayRealNumbers.Length; i++)
+{
     if (maxNumber < arrayRealNumbers[i])
     {
-      maxNumber = arrayRealNumbers[i];
+        maxNumber = arrayRealNumbers[i];
     }
-        if (minNumber > arrayRealNumbers[i])
+    if (minNumber > arrayRealNumbers[i])
     {
-      minNumber = arrayRealNumbers[i];
+        minNumber = arrayRealNumbers[i];
     }
-  }
+}
 
-  double decision = maxNumber - minNumber;
+double decision = maxNumber - minNumber;
 
-  Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {decision}");
+Console.WriteLine($"\nразница между между максимальным ({maxNumber:f2}) и минимальным({minNumber:f2}) элементами: {decision:f4}");
+
+//Math.Round(); округление
+
+
+/* работает не только с массивами
+foreach (double item in collectionArrey) //нет гарантии что пройдет по коллекции по порядку
+{
+    Console.Write($"{item}:f3");
+}
+
+//тоже самое:
+
+for (int i = 0; i < collectionArrey; i++)
+{
+    double item = collectionArrey[i];
+}
+*/
---
+++ HM06/Task01/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int InputInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillArrey(int arreyLen)
+{
+    int[] arrey = new int[arreyLen];
+    for (int i = 0; i < arrey.Length; i++)
+    {
+       arrey[i] = InputInt($" Введите {i + 1} эллемент массива"); 
+    }
+    return arrey;
+}
+
+int SearchPositive (int[] arrey)
+{
+    int count = 0;
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if(arrey[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArrey (int[] arrey)
+{
+    for (int i = 0; i < arrey.Length - 1; i++)
+    {
+        Console.Write($"{arrey[i]}, ");
+    }
+    Console.Write($"{arrey[arrey.Length-1]}");
+    Console.WriteLine();
+}
+
+int lengthArr = InputInt("Введите количество эллементов");
+int[] arrey = FillArrey(lengthArr);
+PrintArrey(arrey);
+Console.WriteLine($"количество положительных чисел в массиве: {SearchPositive(arrey)}");
